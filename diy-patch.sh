8acae7c16b1053f3a261501205100d3127daaf04
a520a <a520ass@hotmail.com>
2023-01-04 15:14:36 +0800

    fix

--- diy-patch.sh
+++ diy-patch.sh
@@ -9,7 +9,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # patch
-          for patch in `find patch/openwrt/${1}/*.patch | LC_ALL=C sort -u`
+          for patch in `find files/patch/openwrt/${1}/*.patch | LC_ALL=C sort -u`
           do
               # detect and remove files which patch will create
               lsdiff -s --strip=1 "${patch}" | grep '^+' | awk '{print $2}' | xargs -I % sh -c 'rm -f %'
