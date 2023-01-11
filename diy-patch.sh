de44365db81edf797b89f51a90029aee07396400
a520a <a520ass@hotmail.com>
2023-01-11 11:21:45 +0800

    m

--- diy-patch.sh
+++ diy-patch.sh
@@ -9,10 +9,16 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # patch
-          for patch in `find files/patch/openwrt/${1}/*.patch | LC_ALL=C sort -u`
-          do
-              # detect and remove files which patch will create
-              lsdiff -s --strip=1 "${patch}" | grep '^+' | awk '{print $2}' | xargs -I % sh -c 'rm -f %'
-              echo "Processing file $patch"
-              patch --batch --silent -p1 -N < "${patch}"
-          done
+for patch in `find files/patch/openwrt/${1}/*.patch | LC_ALL=C sort -u`
+do
+	# detect and remove files which patch will create
+	lsdiff -s --strip=1 "${patch}" | grep '^+' | awk '{print $2}' | xargs -I % sh -c 'rm -f %'
+	echo "==> Processing file $patch"
+	patch --batch --silent -p1 -N < "${patch}" > /dev/null 2>&1
+	if [ $? -eq 0 ]
+	then
+		echo "<== [S] patch file $patch"
+	else
+		echo "<== [F] patch file $patch"	
+	fi
+done
